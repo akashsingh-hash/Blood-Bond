db54a60e07e7b85dc3c90d5057d6686770f935f5
Prajjwal-Saggar <prajjwalsaggar1@gmail.com>
2025-03-04 23:27:45 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-green-400"> 
+      <div>BloodBond</div>
+      <div>United by Blood, Connected for Life</div>
+    </div>
+  );
+};
+
+export default App;
