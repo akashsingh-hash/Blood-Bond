6d310598852dbea81382aff6f09617c911ada59c
Prajjwal-Saggar <prajjwalsaggar1@gmail.com>
2025-03-05 22:55:19 +0530

    The first Commit with file structure and font setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-green-400"> 
+    <div className="flex flex-col items-center justify-center h-screen bg-green-400 font-playwrite">
       <div>BloodBond</div>
       <div>United by Blood, Connected for Life</div>
     </div>
